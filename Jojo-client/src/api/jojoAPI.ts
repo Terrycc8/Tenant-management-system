64e747e4e66ef32ef9e8fa0d9591ad12097ab789
Dun19 <dun1999c@gmail.com>
2023-06-19 12:07:30 +0800

    payment update

--- Jojo-client/src/api/jojoAPI.ts
+++ Jojo-client/src/api/jojoAPI.ts
@@ -3,7 +3,7 @@ import { prepareHeaders } from "./prepareHeaders";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const jojoAPI = createApi({
   reducerPath: "jojoAPI",
-  tagTypes: ["property", "user", "event", "home", "profile"],
+  tagTypes: ["property", "user", "event", "home", "profile", "payment"],
   baseQuery: fetchBaseQuery({
     baseUrl: serverURL,
     prepareHeaders: prepareHeaders,
