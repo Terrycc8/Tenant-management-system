70b445e49be8cbf4ffba1c7cc1166fdd00c28e02
Dun19 <dun1999c@gmail.com>
2023-06-20 04:06:27 +0800

    chatroom update

--- Jojo-server/src/chat/chat.service.ts
+++ Jojo-server/src/chat/chat.service.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Redirect } from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectModel } from 'nest-knexjs';
 import { JWTPayload, ChatroomListOutput } from 'src/types';
 import { Length } from 'class-validator';
+import { ChatroomInputDto } from '../dto/post-chatroom.dto';
+import { routes } from "../../../Jojo-client/src/routes";
 
 @Injectable()
 export class ChatService {
@@ -40,5 +42,50 @@ export class ChatService {
     return 'Hello World!';
   }
 
-  async createChatroom(user: JWTPayload) {}
+  async createChatroom(
+    creator_id: string,
+    receiver_id: string,
+  ) {
+    let chatroom = await this.knex('chatroom')
+    .select('id')
+    .where (creator_id, receiver_id)
+    .first();
+    if (chatroom) {
+      routerLink={routes.chatroom(chatroom.id)
+    }
+    let newChatroom = await this.knex('chatroom')
+    .insert({
+      creator_id,
+      receiver_id,
+    });
+  
+    return newChatroom;
+    }
+  }
+
+  async sendMessage(
+    room_id: string,
+    sender_id: string,
+    content: string,
+    ){
+      let newMessage = await this.knex('message')
+    .insert({
+      room_id,
+      sender_id,
+      content,
+    });
+    return newMessage;
+  }
+
+  async messageById (payload: JWTPayload){
+    let message = this.knex('message')
+    .innerJoin('user as sender', 'sender.id', 'message.sender_id')
+    .select(
+      'message.sender_id',
+      'message.content',
+      'sender.id',
+    )
+    return message;
+  }
 }
+
--- Jojo-server/src/dto/post-chatroom.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class ChatroomInputDto {
-  @IsString()
-  creator_id: string;
-  @IsString()
-  receiver_id: string;
-}
