b0f8e46c8c2cb8fcc9461b3feb22eca3d04b0138
Terry Chan <chanhuiyiu@gmail.comgit config --global user.email chanhuiyiu@gmail.comgit config --global user.email chanhuiyiu@gmail.com>
2023-06-24 22:26:58 +0800

    revise appid

--- Jojo-client/capacitor.config.ts
+++ Jojo-client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.terry-chan.app",
+  appId: "com.terrychan.app",
   appName: "Jojo",
   webDir: "dist",
   server: {
