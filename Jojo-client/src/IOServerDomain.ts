d1483336e3cdb02a508a231519e72352d1d322fc
terry <chanhuiyiu@gmail.com>
2023-06-24 22:06:07 +0800

    save

--- Jojo-client/src/IOServerDomain.ts
+++ Jojo-client/src/IOServerDomain.ts
@@ -5,7 +5,7 @@ if (
   window.origin.includes("s3-website") ||
   window.origin.includes("capacitor://")
 ) {
-  ioServerURL = "ws://jojo.terry-chan.com";
+  ioServerURL = "wss://jojo.terry-chan.com";
 } else if (window.location.href.includes("localhost")) {
   ioServerURL = "ws://localhost:8300";
 } else {
